f1fb73a1803b0bf05b59090425dfa0c9d517a20e
jieyingpeng <jipeng@syr.edu>
2016-04-26 10:05:37 -0700

    a

--- Tree.js
+++ Tree.js
@@ -1,54 +1,56 @@
-var Tree = function(value){
-  var newTree = {};
-  newTree.value = value;
+var Tree = function(value) {
+    var newTree = {};
+    newTree.value = value;
 
 
-  extend(newTree, treeMethods);
-  newTree.children = [];
-  
-  return newTree;
+    extend(newTree, treeMethods);
+    newTree.children = [];
+
+    return newTree;
 };
 
 
 
-var extend = function(to, from){
-  for (var key in from){
-    to[key] = from[key];
-  }
+var extend = function(to, from) {
+    for (var key in from) {
+        to[key] = from[key];
+    }
 };
 
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
+treeMethods.addChild = function(value) {
 
-  var child = Tree(value);
-  this.children.push(child);
+    var child = Tree(value);
+    this.children.push(child);
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target) {
 
-  var found = false;
-  var subroutine = function(node){
-    if ( node.value === target ){
-      found = true;
-      return;
+    var found = false;
+    var walkTree = function(node) {
+        if (node.value === target) {
+            found = true;
+            return;
+        }
+        for (var i = 0; i < node.children.length; i++) {
+            var child = node.children[i];
+            walkTree(child);
+        }
     }
-    for ( var i = 0; i < node.children.length; i++ ){
-      var child = node.children[i];
-      subroutine(child);
-    }
-  }
-  subroutine(this);
-  return found;
+    walkTree(this);
+    return found;
 };
 
-treeMethods.traverse = function(callback){
-  callback(this.value);
+treeMethods.traverse = function(callback) {
+    callback(this.value);
 
-  if ( !this.children ){ return; }
-  for ( var i = 0; i < this.children.length; i++ ){
-    var child = this.children[i];
-    child.traverse(callback);
-  }
+    if (!this.children) {
+        return;
+    }
+    for (var i = 0; i < this.children.length; i++) {
+        var child = this.children[i];
+        child.traverse(callback);
+    }
 };
